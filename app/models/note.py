68e02012ca4e57fa641b890d167ddbec8ac1d8e5
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-09 17:19:36 -0500

    created note model

---
+++ app/models/note.py
@@ -0,0 +1,26 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('videos.id')), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    # video = db.relationship('Video', back_populates='notes')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'video_id': self.video_id,
+            'title': self.title,
+            'description': self.description,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
