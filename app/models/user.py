3946448ebbab2df26d07ab9954e44ca87e72f3a0
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-09 16:49:45 -0500

    revised users model

--- app/models/user.py
+++ app/models/user.py
@@ -10,9 +10,14 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    theme = db.Column(db.String(12))
+    language = db.Column(db.String(2))
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now())
 
     @property
     def password(self):
@@ -29,5 +34,8 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'name': self.name,
+            'email': self.email,
+            'theme': self.theme,
+            'language': self.language
         }
