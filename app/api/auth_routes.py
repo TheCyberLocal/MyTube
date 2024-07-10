b8e80cac9ceee9642c8ab5f772de756a0e19be8e
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-10 16:14:38 -0500

    cleaned auth route syntax

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,46 +1,37 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy import or_
 from app.models import User, db
 from app.forms import LoginForm, SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
-from sqlalchemy import or_
 
 auth_routes = Blueprint('auth', __name__)
 
 @auth_routes.route('/login', methods=['POST'])
 def login():
-    """
-    Logs a user in.
-    """
     form = LoginForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        user = User.query.filter(or_(User.email == form.data['credential'], User.username == form.data['credential'])).first()
-        if user and user.check_password(form.data['password']):
-            login_user(user)
-            return jsonify(user.to_dict())
-    return jsonify({'errors': {'credential': ['Invalid email/username or password.']}}), 401
+        credential = form.credential.data
+        user = User.query.filter(or_(User.email == credential, User.username == credential)).first()
+        login_user(user)
+        return jsonify(user.to_dict())
+    return jsonify({'errors': form.errors}), 401
 
 @auth_routes.route('/logout', methods=['POST'])
 def logout():
-    """
-    Logs a user out.
-    """
     logout_user()
     return jsonify({'message': 'User logged out'})
 
 @auth_routes.route('/signup', methods=['POST'])
 def sign_up():
-    """
-    Creates a new user and logs them in.
-    """
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         cleanUser = User(
-            username=form.data['username'],
-            name=form.data['name'],
-            email=form.data['email'],
-            password=form.data['password']
+            username=form.username.data,
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data
         )
         db.session.add(cleanUser)
         db.session.commit()
