deeada55bffb54f1015f37d8d65669e87a746ca2
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-10 13:21:13 -0500

    removed /auth/ endpoint

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -5,15 +5,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
 
-@auth_routes.route('/')
-def authenticate():
-    """
-    Authenticates a user.
-    """
-    if current_user.is_authenticated:
-        return jsonify(current_user.to_dict())
-    return jsonify({'errors': {'message': 'Unauthorized'}}), 401
-
 @auth_routes.route('/login', methods=['POST'])
 def login():
     """
