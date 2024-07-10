861f3058e826a24486c1b118312b6dc3145e1dfa
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-09 19:01:29 -0500

    updated model init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .highlight import Highlight
+from .note import Note
+from .tag import Tag
+from .video import Video
+from .video_tag import VideoTag
 from .db import environment, SCHEMA
