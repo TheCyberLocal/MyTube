9dd643f3d8f608b092118700265d3c7be47cd46a
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-10 16:27:25 -0500

    created user_update_form

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,34 +1,29 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from app.models import User, db
+from app.forms import UserUpdateForm
 
 user_routes = Blueprint('users', __name__)
 
 @user_routes.route('/<int:id>', methods=['PATCH'])
 @login_required
 def update_user(id):
-    """
-    Update a user profile by ID.
-    """
     if current_user.id != id:
         return jsonify({'errors': 'You do not have permission to update this user.'}), 403
 
-    user = User.query.get_or_404(id)
-    data = request.get_json()
-    user.username = data.get('username', user.username)
-    user.name = data.get('name', user.name)
-    user.email = data.get('email', user.email)
-    user.theme = data.get('theme', user.theme)
-    user.language = data.get('language', user.language)
-    db.session.commit()
-    return jsonify(user.to_dict())
+    form = UserUpdateForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user = User.query.get_or_404(id)
+        user.theme = form.theme.data
+        user.language = form.language.data
+        db.session.commit()
+        return jsonify(user.to_dict())
+    return jsonify({'errors': form.errors}), 400
 
 @user_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_user(id):
-    """
-    Delete a user by ID.
-    """
     if current_user.id != id:
         return jsonify({'errors': 'You do not have permission to delete this user.'}), 403
 
