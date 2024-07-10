2a56402e9209045f6edce7455558ed3cc8cde0b8
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-10 13:16:00 -0500

    removed fetching users from user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -4,24 +4,6 @@ from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
-@user_routes.route('/')
-@login_required
-def users():
-    """
-    Query for all users and returns them in a list of user dictionaries.
-    """
-    users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
-
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    """
-    Query for a user by ID and returns that user in a dictionary.
-    """
-    user = User.query.get_or_404(id)
-    return jsonify(user.to_dict())
-
 @user_routes.route('/<int:id>', methods=['PATCH'])
 @login_required
 def update_user(id):
