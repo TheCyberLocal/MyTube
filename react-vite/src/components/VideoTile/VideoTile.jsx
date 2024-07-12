595a4f3e83d21728f55b2bc70cb378dbe2f22d5d
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-12 12:53:11 -0500

    formatted my-videos tiles

--- react-vite/src/components/VideoTile/VideoTile.jsx
+++ react-vite/src/components/VideoTile/VideoTile.jsx
@@ -7,19 +7,21 @@ function VideoTile({ video }) {
   return (
     <div
       key={video.id}
-      className="video-item"
+      className="video-tile"
       onClick={() => nav(`/videos/${video.id}`)}
     >
-      <img
-        srcSet={`
-      http://img.youtube.com/vi/${video.url}/maxresdefault.jpg 1280w,
-      http://img.youtube.com/vi/${video.url}/hqdefault.jpg 960w,
-      http://img.youtube.com/vi/${video.url}/mqdefault.jpg 640w,
-      http://img.youtube.com/vi/${video.url}/default.jpg 320w
-      `}
-        alt="YouTube Video Thumbnail"
-      />
-      <div>{video.title}</div>
+      <div className="image-container">
+        <img
+          srcSet={`
+          http://img.youtube.com/vi/${video.url}/maxresdefault.jpg 1280w,
+          http://img.youtube.com/vi/${video.url}/hqdefault.jpg 960w,
+          http://img.youtube.com/vi/${video.url}/mqdefault.jpg 640w,
+          http://img.youtube.com/vi/${video.url}/default.jpg 320w
+          `}
+          alt="YouTube Video Thumbnail"
+        />
+      </div>
+      <div className="title">{video.title}</div>
     </div>
   );
 }
