5d4010886b663d5df6b2d6103efb3b75dbd8244f
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-12 17:56:39 -0500

    fixed navigation bar loading flicker

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { thunkLogout } from "../../redux/session";
 import "./Navigation.css";
 
 function Navigation() {
   const sessionUser = useSelector((state) => state.session.user);
+  const isLoading = useSelector((state) => state.session.isLoading);
   const nav = useNavigate();
   const dispatch = useDispatch();
 
@@ -15,17 +15,24 @@ function Navigation() {
 
   return (
     <div className="navigation">
-      {!sessionUser && <h1 onClick={() => nav("/")}>Welcome to MyTube</h1>}
-      {sessionUser && (
-        <div>
-          <button onClick={() => nav("/my-videos")}>My Videos</button>
-          <button onClick={() => dispatch(thunkLogout())}>Add Video</button>
-        </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {!sessionUser && <h1 onClick={() => nav("/")}>Welcome to MyTube</h1>}
+          {sessionUser && (
+            <div>
+              <button onClick={() => nav("/my-videos")}>My Videos</button>
+              <button onClick={() => nav("/add-video")}>Add Video</button>
+              <button onClick={() => dispatch(thunkLogout())}>Logout</button>
+            </div>
+          )}
+          <div>
+            <button onClick={() => nav("/help")}>Help</button>
+            <button onClick={handleProfileClick}>Profile</button>
+          </div>
+        </>
       )}
-      <div>
-        <button onClick={() => nav("/help")}>Help</button>
-        <button onClick={handleProfileClick}>Profile</button>
-      </div>
     </div>
   );
 }
