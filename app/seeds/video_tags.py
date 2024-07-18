681d496a384be779c861e2667a8a430f154c318b
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-18 09:47:48 -0500

    updated video tag seeds

--- app/seeds/video_tags.py
+++ app/seeds/video_tags.py
@@ -4,99 +4,123 @@ from sqlalchemy.sql import text
 video_tag_data = [
     {
         "video_id": 1,
-        "tag_id": 1,
+        "tag_id": 293,
     },
     {
         "video_id": 1,
-        "tag_id": 2,
+        "tag_id": 104,
     },
     {
         "video_id": 2,
-        "tag_id": 1,
+        "tag_id": 348,
     },
     {
         "video_id": 2,
-        "tag_id": 4,
+        "tag_id": 456,
     },
     {
         "video_id": 3,
-        "tag_id": 2,
+        "tag_id": 277,
     },
     {
         "video_id": 3,
-        "tag_id": 6,
+        "tag_id": 199,
     },
     {
         "video_id": 4,
-        "tag_id": 1,
+        "tag_id": 436,
     },
     {
         "video_id": 4,
-        "tag_id": 2,
+        "tag_id": 256,
     },
     {
         "video_id": 5,
-        "tag_id": 9,
+        "tag_id": 348,
     },
     {
         "video_id": 5,
+        "tag_id": 304,
+    },
+    {
+        "video_id": 6,
+        "tag_id": 30,
+    },
+    {
+        "video_id": 6,
+        "tag_id": 201,
+    },
+    {
+        "video_id": 7,
+        "tag_id": 124,
+    },
+    {
+        "video_id": 7,
+        "tag_id": 303,
+    },
+    {
+        "video_id": 8,
+        "tag_id": 139,
+    },
+    {
+        "video_id": 8,
+        "tag_id": 280,
+    },
+    {
+        "video_id": 9,
+        "tag_id": 50,
+    },
+    {
+        "video_id": 9,
+        "tag_id": 469,
+    },
+    {
+        "video_id": 10,
+        "tag_id": 238,
+    },
+    {
+        "video_id": 10,
+        "tag_id": 277,
+    },
+    {
+        "video_id": 11,
+        "tag_id": 366,
+    },
+    {
+        "video_id": 11,
+        "tag_id": 33,
+    },
+    {
+        "video_id": 12,
+        "tag_id": 104,
+    },
+    {
+        "video_id": 12,
+        "tag_id": 204,
+    },
+    {
+        "video_id": 13,
+        "tag_id": 315,
+    },
+    {
+        "video_id": 13,
+        "tag_id": 362,
+    },
+    {
+        "video_id": 14,
+        "tag_id": 256,
+    },
+    {
+        "video_id": 14,
+        "tag_id": 297,
+    },
+    {
+        "video_id": 15,
         "tag_id": 10,
     },
     {
-        "video_id": 6,
+        "video_id": 15,
         "tag_id": 11,
-    },
-    {
-        "video_id": 6,
-        "tag_id": 12,
-    },
-    {
-        "video_id": 7,
-        "tag_id": 13,
-    },
-    {
-        "video_id": 7,
-        "tag_id": 14,
-    },
-    {
-        "video_id": 8,
-        "tag_id": 15,
-    },
-    {
-        "video_id": 8,
-        "tag_id": 16,
-    },
-    {
-        "video_id": 9,
-        "tag_id": 17,
-    },
-    {
-        "video_id": 9,
-        "tag_id": 18,
-    },
-    {
-        "video_id": 10,
-        "tag_id": 19,
-    },
-    {
-        "video_id": 10,
-        "tag_id": 20,
-    },
-    {
-        "video_id": 11,
-        "tag_id": 21,
-    },
-    {
-        "video_id": 11,
-        "tag_id": 22,
-    },
-    {
-        "video_id": 12,
-        "tag_id": 23,
-    },
-    {
-        "video_id": 12,
-        "tag_id": 24,
     }
 ]
 
