0f7e45bfda1c327d41df65b5515e2514bfd47870
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-19 17:39:59 -0500

    updated help page with latest i18n version

--- react-vite/src/components/HelpPage/HelpPage.jsx
+++ react-vite/src/components/HelpPage/HelpPage.jsx
@@ -1,129 +1,130 @@
 import "./HelpPage.css";
-import { getTranslation as t } from "../../utils";
+import { getTranslation } from "../../utils";
 import { useSelector } from "react-redux";
 
 function HelpPage() {
   const { language: lang } = useSelector((state) => state.session);
+  const t = getTranslation(lang);
 
   return (
     <div id="help-page">
-      <h1>{t(lang, "help_page_title")}</h1>
-      <p>{t(lang, "help_page_welcome")}</p>
+      <h1>{t("help_page_title")}</h1>
+      <p>{t("help_page_welcome")}</p>
 
       <div className="segment">
-        <h2>{t(lang, "help_page_users")}</h2>
+        <h2>{t("help_page_users")}</h2>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_users_q1")}</h3>
-          <p>{t(lang, "help_page_users_a1")}</p>
+          <h3>{t("help_page_users_q1")}</h3>
+          <p>{t("help_page_users_a1")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_users_q2")}</h3>
-          <p>{t(lang, "help_page_users_a2")}</p>
+          <h3>{t("help_page_users_q2")}</h3>
+          <p>{t("help_page_users_a2")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_users_q3")}</h3>
-          <p>{t(lang, "help_page_users_a3")}</p>
+          <h3>{t("help_page_users_q3")}</h3>
+          <p>{t("help_page_users_a3")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_users_q4")}</h3>
-          <p>{t(lang, "help_page_users_a4")}</p>
+          <h3>{t("help_page_users_q4")}</h3>
+          <p>{t("help_page_users_a4")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_users_q5")}</h3>
-          <p>{t(lang, "help_page_users_a5")}</p>
+          <h3>{t("help_page_users_q5")}</h3>
+          <p>{t("help_page_users_a5")}</p>
         </div>
       </div>
 
       <div className="segment">
-        <h2>{t(lang, "help_page_videos")}</h2>
+        <h2>{t("help_page_videos")}</h2>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q1")}</h3>
-          <p>{t(lang, "help_page_videos_a1")}</p>
+          <h3>{t("help_page_videos_q1")}</h3>
+          <p>{t("help_page_videos_a1")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q2")}</h3>
-          <p>{t(lang, "help_page_videos_a2")}</p>
+          <h3>{t("help_page_videos_q2")}</h3>
+          <p>{t("help_page_videos_a2")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q3")}</h3>
-          <p>{t(lang, "help_page_videos_a3")}</p>
+          <h3>{t("help_page_videos_q3")}</h3>
+          <p>{t("help_page_videos_a3")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q4")}</h3>
-          <p>{t(lang, "help_page_videos_a4")}</p>
+          <h3>{t("help_page_videos_q4")}</h3>
+          <p>{t("help_page_videos_a4")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q5")}</h3>
-          <p>{t(lang, "help_page_videos_a5")}</p>
+          <h3>{t("help_page_videos_q5")}</h3>
+          <p>{t("help_page_videos_a5")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_videos_q6")}</h3>
-          <p>{t(lang, "help_page_videos_a6")}</p>
-        </div>
-      </div>
-
-      <div className="segment">
-        <h2>{t(lang, "help_page_notes")}</h2>
-
-        <div className="question-container">
-          <h3>{t(lang, "help_page_notes_q1")}</h3>
-          <p>{t(lang, "help_page_notes_a1")}</p>
-        </div>
-
-        <div className="question-container">
-          <h3>{t(lang, "help_page_notes_q2")}</h3>
-          <p>{t(lang, "help_page_notes_a2")}</p>
-        </div>
-
-        <div className="question-container">
-          <h3>{t(lang, "help_page_notes_q3")}</h3>
-          <p>{t(lang, "help_page_notes_a3")}</p>
-        </div>
-
-        <div className="question-container">
-          <h3>{t(lang, "help_page_notes_q4")}</h3>
-          <p>{t(lang, "help_page_notes_a4")}</p>
+          <h3>{t("help_page_videos_q6")}</h3>
+          <p>{t("help_page_videos_a6")}</p>
         </div>
       </div>
 
       <div className="segment">
-        <h2>{t(lang, "help_page_highlights")}</h2>
+        <h2>{t("help_page_notes")}</h2>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_highlights_q1")}</h3>
-          <p>{t(lang, "help_page_highlights_a1")}</p>
+          <h3>{t("help_page_notes_q1")}</h3>
+          <p>{t("help_page_notes_a1")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_highlights_q2")}</h3>
-          <p>{t(lang, "help_page_highlights_a2")}</p>
+          <h3>{t("help_page_notes_q2")}</h3>
+          <p>{t("help_page_notes_a2")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_highlights_q3")}</h3>
-          <p>{t(lang, "help_page_highlights_a3")}</p>
+          <h3>{t("help_page_notes_q3")}</h3>
+          <p>{t("help_page_notes_a3")}</p>
         </div>
 
         <div className="question-container">
-          <h3>{t(lang, "help_page_highlights_q4")}</h3>
-          <p>{t(lang, "help_page_highlights_a4")}</p>
+          <h3>{t("help_page_notes_q4")}</h3>
+          <p>{t("help_page_notes_a4")}</p>
         </div>
       </div>
 
-      <p>{t(lang, "help_page_guide")}</p>
+      <div className="segment">
+        <h2>{t("help_page_highlights")}</h2>
 
-      <p>{t(lang, "help_page_happy_organizing")}</p>
+        <div className="question-container">
+          <h3>{t("help_page_highlights_q1")}</h3>
+          <p>{t("help_page_highlights_a1")}</p>
+        </div>
+
+        <div className="question-container">
+          <h3>{t("help_page_highlights_q2")}</h3>
+          <p>{t("help_page_highlights_a2")}</p>
+        </div>
+
+        <div className="question-container">
+          <h3>{t("help_page_highlights_q3")}</h3>
+          <p>{t("help_page_highlights_a3")}</p>
+        </div>
+
+        <div className="question-container">
+          <h3>{t("help_page_highlights_q4")}</h3>
+          <p>{t("help_page_highlights_a4")}</p>
+        </div>
+      </div>
+
+      <p>{t("help_page_guide")}</p>
+
+      <p>{t("help_page_happy_organizing")}</p>
     </div>
   );
 }
