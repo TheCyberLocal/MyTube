c5682febadce3bd20bc13ef49fedc7648808fd0d
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-20 12:05:47 -0500

    created pt lib in i18n

--- react-vite/src/utils/i18n/i18n.js
+++ react-vite/src/utils/i18n/i18n.js
@@ -1,4 +1,23 @@
-// i18n.js
+// English en
+// Spanish es
+// Portuguese pt
+// French fr
+// German de
+// Italian it
+// Japanese ja
+// Korean ko
+// Mandarin zh
+// Russian ru
+// Arabic ar
+// Hindi hi
+// Bengali bn
+// Indonesian id
+// Malay ms
+// Thai th
+// Vietnamese vi
+// Turkish tr
+// Polish pl
+
 async function loadTranslation(lang) {
   try {
     const module = await import(`./lib/${lang}`);
