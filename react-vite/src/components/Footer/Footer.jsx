8ab095bda301ccc5cd76cf79806d82041f32e458
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-26 10:53:11 -0500

    added footer but blocks bottom of page

---
+++ react-vite/src/components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import {
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaUser,
+  FaBriefcase,
+} from "react-icons/fa";
+import "./Footer.css";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div id="footer-copyright">
+        © {currentYear} Timothy Macfarlane. All rights reserved.
+      </div>
+      <div id="footer-icons">
+        <a
+          href="https://thecyberlocal.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaUser size={24} />
+        </a>
+        <a
+          href="https://docs.google.com/document/d/1LT2Iz5d83Csb5v13ttEb-NLPQ6O8Cr8O64vQ4Fcqw0k"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaBriefcase size={24} />
+        </a>
+        <a
+          href="https://linkedin.com/in/tzm01"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://github.com/TheCyberLocal"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={24} />
+        </a>
+        <a href="mailto:timdiscovers@gmail.com">
+          <FaEnvelope size={24} />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
