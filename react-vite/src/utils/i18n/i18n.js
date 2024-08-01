a198f331f15b3e72c952cac2e4abfdf6be3d583f
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-31 22:59:23 -0500

    refactored i18n dynamic imports to not use template literals

--- react-vite/src/utils/i18n/i18n.js
+++ react-vite/src/utils/i18n/i18n.js
@@ -1,15 +1,38 @@
+const dynamicModules = {
+  ar: import("./lib/ar"),
+  bn: import("./lib/bn"),
+  de: import("./lib/de"),
+  en: import("./lib/en"),
+  es: import("./lib/es"),
+  fr: import("./lib/fr"),
+  hi: import("./lib/hi"),
+  id: import("./lib/id"),
+  it: import("./lib/it"),
+  ja: import("./lib/ja"),
+  ko: import("./lib/ko"),
+  ms: import("./lib/ms"),
+  pl: import("./lib/pl"),
+  pt: import("./lib/pt"),
+  ru: import("./lib/ru"),
+  th: import("./lib/th"),
+  tr: import("./lib/tr"),
+  ur: import("./lib/ur"),
+  vi: import("./lib/vi"),
+  zh: import("./lib/zh"),
+};
+
 async function loadTranslation(lang) {
   try {
-    const module = await import(`./lib/${lang}`);
+    const module = await dynamicModules[lang];
     return module.default;
   } catch {
-    const fallbackModule = await import(`./lib/en`);
+    const fallbackModule = await dynamicModules["en"];
     return fallbackModule.default;
   }
 }
 
 export async function getTranslation(lang) {
-  const translations = await loadTranslation(lang || "en");
+  const translations = await loadTranslation(lang);
   return (phrase, arg) => {
     return translations[phrase].replace("{var}", arg);
   };
